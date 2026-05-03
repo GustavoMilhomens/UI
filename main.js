c264c9e652b5504b8e4c69e72c6d00ee428de7b8
Gustavo Milhomens <gustavo.santos23@estudante.ifto.edu.br>
2026-05-02 21:28:25 -0300

    concerto no referencial de pastas

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-// faz com que a linguagem fique em portugues 
+//* faz com que a linguagem fique em portugues 
 document.documentElement.lang="pt-br";
 
-// informa o caminho principal
-const base = window.location.origin + "/" + window.location.pathname.split("/")[1];  
+//* informa o caminho principal
+// ! comando provisorio 
+const relative_path = window.location.pathname.split('/'); //? localiza o local atual do arquivo e transforma em uma lista 
+relative_path[0] = window.location.origin; //? adiciona o local de origem da janela na primeira posição 
 
+while(relative_path.at(-1) != 'UI'){ //? verifica se valor da ultima "casa" é "UI"
+    relative_path.pop(); //? remove valor da ultima "casa" se não for "UI"
+}
+const base = relative_path.join("/"); //? junta tudo e coloca barra entre os valores 
+
+
+//* adiciona o os links das atividades no menu de acordo com a pasta base 
 function add_link_menu() {
-    //? identifica o butão btn_config
+    //* identifica o butão btn_config
     const btn = document.getElementById("btn_config");
 
-    //? cria o datails 
+    //* cria o datails 
     const details = document.createElement("details");
-    //? implementa os elementos referenciados corretamente
+    //* implementa os elementos referenciados corretamente
     details.innerHTML = `
         <summary>Atividades</summary>
         <a href="${base}/index.html">Introdução da Disciplina</a><br>
@@ -30,46 +39,47 @@ function add_link_menu() {
         </details>
         `;
 
-    //? Insere antes do botão
+    //* Insere antes do botão
     btn.parentElement.insertBefore(details, btn);
 
     //! ===========================|visualizar erros|================================
     console.log('referencia de atividades adicionado')
 }
 
-// adiciona o style e as divs, menu e config, em todos os arquivos que receberem o main.js
+//* adiciona o style e as divs, menu e config, em todos os arquivos que receberem o main.js
 async function add_element(){
     //* add style 
     const link = document.createElement('link'); //? cria uma variavel que recebe o elemento link
     link.rel = 'stylesheet'; //? configura 
     link.href = base + '/style.css'; //? referencia o css
     document.head.append(link); //? adiciona o elemento
-    //! o append() adiciona no final 
+    
+    //$ o append() adiciona no final 
 
     //* add div config
     const div_config = document.createElement('div'); //? cria a varivel do elemento
     div_config.id = 'config_div'; //? adiciona o id
     document.body.prepend(div_config); //? adiciona a div ao body 
-    //! o prepend() adiciona no inicio
+    //$ o prepend() adiciona no inicio
 
     //* add div menu
     const div_menu = document.createElement('div'); 
     div_menu.id = 'menu_div'; 
     document.body.prepend(div_menu);
 
-    // espera as divs carregarem para adicionar os complementos 
+    //* espera as divs carregarem para adicionar os complementos 
     await include_complement();
     
-    // adiciona os links de atividades da barra menu 
+    //* adiciona os links de atividades da barra menu 
     await add_link_menu();
 
     //! ===========================|visualizar erros|================================
     console.log('style.css, menu_div e config_div adicionado')
 }
 
-// procura o arquivo de menu 
+//* procura o arquivo de menu 
 async function include_complement() {
-    // coleta o caminho do menu 
+    //* coleta o caminho do menu 
     const menu_bar = await fetch( base + "/complementos/barra_menu.html");
     document.getElementById('menu_div').innerHTML = await menu_bar.text();
     
@@ -81,39 +91,39 @@ async function include_complement() {
     console.log('complementos adicionados')
 }
 
-// abre e fecha o menu 
+//* abre e fecha o menu 
 function open_close_menu(){
         const menu = document.getElementById('menu_div_bar');
         menu.classList.toggle('active') 
     }
 
-// abre e fecha a caixa de preferencias 
+//* abre e fecha a caixa de preferencias 
 function open_close_config(){
         const prefe = document.getElementById('config_div_box');
         prefe.classList.toggle('active') 
 }
 
-// deixa o sistema funcionando de forma assincrona 
+//* deixa o sistema funcionando de forma assincrona 
 async function start() {
-    // cria um botão onde de para mudar o tema do site
+    //* cria um botão onde de para mudar o tema do site
     const btn_theme = document.getElementById("btn_theme");
     const var_theme = localStorage.getItem("theme");
 
-    // verifica a preferencia do usuario 
+    //* verifica a preferencia do usuario 
     if (var_theme === "dark"){
         document.body.classList.add('dark_theme');
     }
 
-    // adiciona o tema com o click
+    //* adiciona o tema com o click
     btn_theme.addEventListener("click", function(){
         document.body.classList.toggle('dark_theme');
 
-        // ve se o atual é dark e salva a preferencia
+        //* ve se o atual é dark e salva a preferencia
         let theme = 'light';
         if (document.body.classList.contains("dark_theme")){
             theme = "dark";
         }
-        // salva o tema
+        //* salva o tema
         localStorage.setItem('theme', theme); 
     });
 
@@ -121,7 +131,7 @@ async function start() {
     console.log('tema dark adicionado')
 }
 
-// chama as funções
+//* chama as funções
 async function init(){
     await add_element();
     await start();
